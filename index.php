f36a5a920a5e209a60b8d192eab785e9e5c96f51
Elisaragao <elisangelaaragao644@gmail.com>
2024-09-11 08:43:33 -0300

    Codificação inicial e H1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git e git hub</title>
+</head>
+<body>
+   <h1> Versionamento com GIT/GIT HUB</h1>
+  <h1>i love the smiths</h1>
+</body>
+</html>
