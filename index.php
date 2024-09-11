d69e4a31310dff898abf5bcf54ba8c530ee2a059
Elisaragao <elisangelaaragao644@gmail.com>
2024-09-11 09:31:14 -0300

    Criado o estilo CSS

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Git e git hub</title>
+  <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
    <h1> Versionamento com GIT/GIT HUB</h1>
